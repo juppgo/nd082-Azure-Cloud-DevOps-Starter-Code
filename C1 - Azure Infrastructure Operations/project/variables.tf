c474006e029d6f10571593a4401d6ca926cccfbf
juppgo <50930219+juppgo@users.noreply.github.com>
2023-10-15 11:15:18 +0200

    add default value to location variable

--- C1 - Azure Infrastructure Operations/project/variables.tf
+++ C1 - Azure Infrastructure Operations/project/variables.tf
@@ -5,6 +5,7 @@ variable "prefix" {
 
 variable "location" {
   description = "The Azure location where all resources in this example should be created"
+  default = "East US"
 }
 
 variable "username" {
