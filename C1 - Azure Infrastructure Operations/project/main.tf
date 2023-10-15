d2245723623855a6d1c9ccb9a4cfe93955377e12
juppgo <50930219+juppgo@users.noreply.github.com>
2023-10-15 10:35:36 +0200

    add nsg, load balance resources, managed disks; add tags to resources; add count variables for multiple vm deployment

--- C1 - Azure Infrastructure Operations/project/main.tf
+++ C1 - Azure Infrastructure Operations/project/main.tf
@@ -11,17 +11,21 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  tags = {
+    project = "${var.prefix}"
+  }
 }
 
 resource "azurerm_subnet" "sn" {
   name                 = "${var.prefix}-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/16"]
+  address_prefixes     = ["10.0.0.0/16"]
 }
 
 resource "azurerm_network_interface" "nic" {
-  name                = "${var.prefix}-nic"
+  count = var.vm_number
+  name                = "${var.prefix}-nic-${count.index}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
 
@@ -30,12 +34,60 @@ resource "azurerm_network_interface" "nic" {
     subnet_id                     = azurerm_subnet.sn.id
     private_ip_address_allocation = "Dynamic"
   }
+  tags = {
+    project = "${var.prefix}"
+  }
 }
 
 resource "azurerm_network_security_group" "nsg" {
   name                = "${var.prefix}-nsg"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
+  tags = {
+    project = "${var.prefix}"
+  }
+}
+
+resource "azurerm_network_security_rule" "nsr-inbound" {
+  name                        = "allow_internal_inbound"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "10.0.0.0/16"
+  destination_address_prefix  = "10.0.0.0/16"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.rg.name
+}
+
+resource "azurerm_network_security_rule" "nsr-outbound" {
+  name                        = "allow_internal_outbound"
+  priority                    = 101
+  direction                   = "Outbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "10.0.0.0/16"
+  destination_address_prefix  = "10.0.0.0/16"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.rg.name
+}
+
+resource "azurerm_network_security_rule" "nsr-inbound" {
+  name                        = "deny_external_inbound"
+  priority                    = 102
+  direction                   = "Inbound"
+  access                      = "Deny"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.rg.name
 }
 
 
@@ -45,8 +97,8 @@ resource "azurerm_public_ip" "pip" {
   location            = azurerm_resource_group.rg.location
   allocation_method   = "Static"
 
-  tags = {
-    environment = "Production"
+tags = {
+    project = "${var.prefix}"
   }
 }
 
@@ -59,17 +111,36 @@ resource "azurerm_lb" "lb" {
     name                 = "PublicIPAddress"
     public_ip_address_id = azurerm_public_ip.pip.id
   }
+
+  tags = {
+    project = "${var.prefix}"
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "lb_ap" {
+  loadbalancer_id = azurerm_lb.lb.id
+  name            = "${var.prefix}-BackEndAddressPool"
+}
+
+resource "azurerm_network_interface_backend_address_pool_association" "nic_b_ap_a" {
+  count = var.vm_number
+  network_interface_id    = azurerm_network_interface.nic.id
+  ip_configuration_name   = "${var.prefix}-testconfiguration1"
+  backend_address_pool_id = element(azurerm_lb_backend_address_pool.lb_ap.*.id, count.index)
 }
 
 resource "azurerm_availability_set" "as" {
   name                = "${var.prefix}-aset"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-
+  tags = {
+    project = "${var.prefix}"
+  }
 }
 
-resource "azurerm_linux_virtual_machine" "main" {
-  name                            = "${var.prefix}-vm"
+resource "azurerm_linux_virtual_machine" "vm" {
+  count = var.vm_number
+  name                            = "${var.prefix}-vm-${count.index}"
   resource_group_name             = azurerm_resource_group.rg.name
   location                        = azurerm_resource_group.rg.location
   size                            = "Standard_D2s_v3"
@@ -77,7 +148,7 @@ resource "azurerm_linux_virtual_machine" "main" {
   admin_password                  = var.password
   disable_password_authentication = false
   network_interface_ids = [
-    azurerm_network_interface.main.id,
+    azurerm_network_interface.nic[count.index].id,
   ]
   source_image_id = data.azurerm_image.custom_image.id
 
@@ -85,16 +156,21 @@ resource "azurerm_linux_virtual_machine" "main" {
     storage_account_type = "Standard_LRS"
     caching              = "ReadWrite"
   }
-
+  tags = {
+    project = "${var.prefix}"
+  }
 }
 
 resource "azurerm_managed_disk" "md" {
-  name                 = "${var.prefix}-md"
+  count = var.vm_number
+  name                 = "${var.prefix}-md-${count.index}"
   location             = azurerm_resource_group.rg.location
   resource_group_name  = azurerm_resource_group.rg.name
   storage_account_type = "Standard_LRS"
   create_option        = "Empty"
   disk_size_gb         = "1"
-
+  tags = {
+    project = "${var.prefix}"
+  }
 
 }
--- C1 - Azure Infrastructure Operations/project/variables.tf
+++ C1 - Azure Infrastructure Operations/project/variables.tf
@@ -1,6 +1,6 @@
 variable "prefix" {
   description = "The prefix used for all resources in this example"
-  default = "udacity"
+  default     = "udacity-project-c1"
 }
 
 variable "location" {
@@ -8,11 +8,17 @@ variable "location" {
 }
 
 variable "username" {
-    description = "Username to login to VM"
-    default = "adminuser"
+  description = "Username to login to VM"
+  default     = "adminuser"
 }
 
 variable "password" {
-    description = "passwrod to login to vm"
-    default = "54f3p455w0rd"
+  description = "passwrod to login to vm"
+  default     = "54f3p455w0rd"
+}
+
+variable "vm_number" {
+  description = "number of VMs to be created"
+  default     = 4
+  type        = number
 }
